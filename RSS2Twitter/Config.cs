335caa846f00e34e17e26143d35e28bc3a182b1f
Passive <passivemodding@gmail.com>
2017-12-12 21:04:35 +1100

    Minor Cleanup and updates

---
+++ RSS2Twitter/Config.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RSS2Twitter
+{
+    public class Config
+    {
+        [JsonIgnore] public static readonly string Appdir = AppContext.BaseDirectory;
+
+
+        public static string ConfigPath = Path.Combine(AppContext.BaseDirectory, "setup/config.json");
+
+        public string ConsumerKey { get; set; } = "";
+        public string ConsumerKeySecret { get; set; } = "";
+        public string AccessToken { get; set; } = "";
+        public string AccessTokenSecret { get; set; } = "";
+        public string RssUrl { get; set; } = "";
+        public int Frequency { get; set; } = 10;
+
+
+        public void Save(string dir = "setup/config.json")
+        {
+            var file = Path.Combine(Appdir, dir);
+            File.WriteAllText(file, ToJson());
+        }
+
+        public static Config Load(string dir = "setup/config.json")
+        {
+            var file = Path.Combine(Appdir, dir);
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(file));
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        public static void CheckExistence()
+        {
+            try
+            {
+                var accessToken = Load().AccessToken;
+                if (string.IsNullOrEmpty(accessToken))
+                    throw new NullReferenceException();
+            }
+            catch
+            {
+                {
+                    Console.WriteLine("Run (Y for run, N for setup Config)");
+
+                    Console.WriteLine("Y or N: ");
+                    var res = Console.ReadKey();
+                    if (res.KeyChar == 'N' || res.KeyChar == 'n')
+                        File.Delete("setup/config.json");
+
+                    if (!Directory.Exists(Path.Combine(AppContext.BaseDirectory, "setup/")))
+                        Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, "setup/"));
+                }
+
+
+                if (!File.Exists(ConfigPath))
+                {
+                    var cfg = new Config();
+
+                    Console.WriteLine(
+                        "Please go to https://apps.twitter.com/ and create a new app\n" +
+                        "Once Created, go to 'Keys and Access Tokens'\n" +
+                        "Make sure you have generated your access token\n" +
+                        "Press Any Key to Continue");
+                    Console.ReadKey();
+                    Console.Write("Please enter your Consumer Key (API Key): \n");
+                    cfg.ConsumerKey = Console.ReadLine();
+                    Console.Write("Please enter your Consumer Key Secret (API Secret): \n");
+                    cfg.ConsumerKeySecret = Console.ReadLine();
+                    Console.Write("Please enter your Access Token: ");
+                    cfg.AccessToken = Console.ReadLine();
+                    Console.Write("Please enter your Access Token Secret: \n");
+                    cfg.AccessTokenSecret = Console.ReadLine();
+
+                    Console.Write("Please enter your Desired RSS Url: \n");
+                    cfg.RssUrl = Console.ReadLine();
+
+                    Console.Write("Please enter your desired frequency of RSS Update Checks \n" +
+                                  "ie. 5 = RSS Updates are checked every 5 minutes: \n");
+                    cfg.Frequency = Convert.ToInt32(Console.ReadLine());
+
+                    cfg.Save();
+                }
+            }
+        }
+    }
+}
--- RSS2Twitter/Program.cs
+++ RSS2Twitter/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using Newtonsoft.Json;
 
 namespace RSS2Twitter
 {
@@ -21,21 +19,41 @@ namespace RSS2Twitter
         public async Task Start()
         {
             Config.CheckExistence();
-            //var response = await twitter.Tweet("This is my first automated tweet!");
-            //Console.WriteLine(response);
+
+            var consumerKey = Config.Load().ConsumerKey;
+            var consumerKeySecret = Config.Load().ConsumerKeySecret;
+            var accessToken = Config.Load().AccessToken;
+            var accessTokenSecret = Config.Load().AccessTokenSecret;
+            var twitter = new TwitterApi(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
+
+            while (true)
+            {
+                var feedItems = (await Index(Config.Load().RssUrl)).Where(x =>
+                    x.PublishDate.ToUniversalTime() + TimeSpan.FromMinutes(Config.Load().Frequency) > DateTime.UtcNow);
+
+                foreach (var item in feedItems)
+                    try
+                    {
+                        //if (item.Title.ToLower().Contains("sock"))
+                        //    throw new Exception("Proxy");
+
+                        await twitter.Tweet($"{item.Title}\n" +
+                                            $"{item.Link}");
+                        Console.WriteLine(
+                            $"{item.Title}                                                                    "
+                                .Substring(0, 30));
+
+                        await Task.Delay(1000);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
 
 
-            await DoRss();
-        }
-
-        public class FeedItem
-        {
-            public string Link { get; set; }
-            public string Title { get; set; }
-            public string Content { get; set; }
-            public DateTime PublishDate { get; set; }
-            public string RssRootUrl { get; set; }
-            public string PostRootUrl { get; set; }
+                await Task.Delay(1000 * 60 * Config.Load().Frequency);
+            }
+            // ReSharper disable once FunctionNeverReturns
         }
 
         public async Task<List<FeedItem>> Index(string url)
@@ -53,97 +71,59 @@ namespace RSS2Twitter
                 var doc = XDocument.Parse(rs2);
                 if (doc.Root == null) return articles;
 
-                var feedItems = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel")
-                        .Elements().Where(i => i.Name.LocalName == "item")
-                                select new FeedItem
-                                {
-                                    Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
-                                    Link = item.Elements().First(i => i.Name.LocalName == "link").Value,
-                                    PublishDate = ParseDate(item.Elements().First(i => i.Name.LocalName == "pubDate").Value),
-                                    Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
-                                    RssRootUrl = $"{new Uri(feedUrl).Scheme}://{new Uri(feedUrl).Host}",
-                                    PostRootUrl =
-                                        $"{new Uri(item.Elements().First(i => i.Name.LocalName == "link").Value).Scheme}://{new Uri(item.Elements().First(i => i.Name.LocalName == "link").Value).Host}"
-                                };
+                //Convert each item into a FeedItem
+                var feedItems = doc.Root.Descendants()
+                    .First(i => i.Name.LocalName == "channel")
+                    .Elements()
+                    .Where(i => i.Name.LocalName == "item")
+                    .Select(item => new FeedItem
+                    {
+                        Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                        Link = item.Elements().First(i => i.Name.LocalName == "link").Value,
+                        PublishDate =
+                            DateTime.TryParse(item.Elements().First(i => i.Name.LocalName == "pubDate").Value,
+                                out var result)
+                                ? result
+                                : DateTime.MinValue,
+                        Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                        RssRootUrl = $"{new Uri(feedUrl).Scheme}://{new Uri(feedUrl).Host}",
+                        PostRootUrl =
+                            $"{new Uri(item.Elements().First(i => i.Name.LocalName == "link").Value).Scheme}://{new Uri(item.Elements().First(i => i.Name.LocalName == "link").Value).Host}"
+                    });
                 articles = feedItems.ToList();
             }
 
             return articles;
         }
 
-        private static DateTime ParseDate(string date)
+        /// <summary>
+        /// This is the object for each RSS Post
+        /// it contains all relevant info for Each Post
+        /// </summary>
+        public class FeedItem
         {
-            return DateTime.TryParse(date, out var result) ? result : DateTime.MinValue;
-        }
-
-        //public static List<RssObject> RssFeeds = new List<RssObject>();
-
-        //public class RssObject
-        //{
-        //    public string Formatting { get; set; } = "$posttitle\n" +
-        //                                             "$postlink";
-        //    public string RssUrl { get; set; }
-        //}
-
-        private async Task DoRss()
-        {
-            var consumerKey = Config.Load().ConsumerKey;
-            var consumerKeySecret = Config.Load().ConsumerKeySecret;
-            var accessToken = Config.Load().AccessToken;
-            var accessTokenSecret = Config.Load().AccessTokenSecret;
-            var twitter = new TwitterApi(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
-
-            while (true)
-            {
-                var feedItems = (await Index(Config.Load().RssUrl)).Where(x =>
-                    x.PublishDate.ToUniversalTime() + TimeSpan.FromMinutes(1000000) > DateTime.UtcNow);
-
-                foreach (var item in feedItems)
-                {
-                    try
-                    {
-                        /*var desc = Regex.Replace(item.Content, "<.*?>", "");
-                        var d2 = feed.Formatting
-                            .Replace("$posttitle", item.Title)
-                            .Replace("$postlink", item.Link)
-                            //.Replace("$postcontent", desc)
-                            .Replace("$postrssroot", item.RssRootUrl)
-                            .Replace("$postroot", item.PostRootUrl)
-                            .Replace("$rssurl", feed.RssUrl);*/
-
-                        if (item.Title.ToLower().Contains("sock"))
-                        {
-                            throw new Exception("Proxy");
-                        }
-
-                        await twitter.Tweet($"{item.Title}\n" +
-                                            $"{item.Link}");
-                        Console.WriteLine($"{item.Title}                                                                    ".Substring(0, 30));
-
-                        await Task.Delay(1000);
-
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-
-
-
-
-                await Task.Delay(1000 * 60 * 10);
-            }
-            // ReSharper disable once FunctionNeverReturns
+            public string Link { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public DateTime PublishDate { get; set; }
+            public string RssRootUrl { get; set; }
+            public string PostRootUrl { get; set; }
         }
 
         public class TwitterApi
         {
             public const string TwitterApiBaseUrl = "https://api.twitter.com/1.1/";
             public readonly string ConsumerKey, ConsumerKeySecret, AccessToken, AccessTokenSecret;
-            public readonly HMACSHA1 SigHasher;
             public readonly DateTime EpochUtc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            public readonly HMACSHA1 SigHasher;
 
+            /// <summary>
+            /// Creates an object for sending tweets to Twitter using Single-user OAuth.
+            /// 
+            /// Get your access keys by creating an app at apps.twitter.com then visiting the
+            /// "Keys and Access Tokens" section for your app. They can be found under the
+            /// "Your Access Token" heading.
+            /// </summary>
             public TwitterApi(string consumerKey, string consumerKeySecret, string accessToken,
                 string accessTokenSecret)
             {
@@ -157,6 +137,9 @@ namespace RSS2Twitter
                         new ASCIIEncoding().GetBytes(string.Format("{0}&{1}", consumerKeySecret, accessTokenSecret)));
             }
 
+            /// <summary>
+            /// Sends a tweet with the supplied text and returns the response from the Twitter API.
+            /// </summary>
             public Task<string> Tweet(string text)
             {
                 var data = new Dictionary<string, string>
@@ -168,12 +151,15 @@ namespace RSS2Twitter
                 return SendRequest("statuses/update.json", data);
             }
 
+            /// <summary>
+            /// Send HTTP Request and return the response.
+            /// </summary>
             private Task<string> SendRequest(string url, Dictionary<string, string> data)
             {
                 var fullUrl = TwitterApiBaseUrl + url;
 
                 // Timestamps are in seconds since 1/1/1970.
-                var timestamp = (int)((DateTime.UtcNow - EpochUtc).TotalSeconds);
+                var timestamp = (int) (DateTime.UtcNow - EpochUtc).TotalSeconds;
 
                 // Add all the OAuth headers we'll need to use when constructing the hash.
                 data.Add("oauth_consumer_key", ConsumerKey);
@@ -195,6 +181,9 @@ namespace RSS2Twitter
                 return SendRequest(fullUrl, oAuthHeader, formData);
             }
 
+            /// <summary>
+            /// Generate an OAuth signature from OAuth header values.
+            /// </summary>
             private string GenerateSignature(string url, Dictionary<string, string> data)
             {
                 var sigString = string.Join(
@@ -217,6 +206,10 @@ namespace RSS2Twitter
                     SigHasher.ComputeHash(new ASCIIEncoding().GetBytes(fullSigData)));
             }
 
+
+            /// <summary>
+            /// Generate the raw OAuth HTML header from the values (including signature).
+            /// </summary>
             private static string GenerateOAuthHeader(Dictionary<string, string> data)
             {
                 return "OAuth " + string.Join(
@@ -240,91 +233,6 @@ namespace RSS2Twitter
                     var respBody = await httpResp.Content.ReadAsStringAsync();
 
                     return respBody;
-                }
-            }
-        }
-
-    }
-
-    public class Config
-    {
-        [JsonIgnore] public static readonly string Appdir = AppContext.BaseDirectory;
-
-
-        public static string ConfigPath = Path.Combine(AppContext.BaseDirectory, "setup/config.json");
-
-        public string ConsumerKey { get; set; } = "";
-        public string ConsumerKeySecret { get; set; } = "";
-        public string AccessToken { get; set; } = "";
-        public string AccessTokenSecret { get; set; } = "";
-        public string RssUrl { get; set; } = "";
-
-
-        public void Save(string dir = "setup/config.json")
-        {
-            var file = Path.Combine(Appdir, dir);
-            File.WriteAllText(file, ToJson());
-        }
-
-        public static Config Load(string dir = "setup/config.json")
-        {
-            var file = Path.Combine(Appdir, dir);
-            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(file));
-        }
-
-        public string ToJson()
-        {
-            return JsonConvert.SerializeObject(this, Formatting.Indented);
-        }
-
-        public static void CheckExistence()
-        {
-            try
-            {
-                var accessToken = Load().AccessToken;
-                if (string.IsNullOrEmpty(accessToken))
-                {
-                    throw new NullReferenceException();
-                }
-            }
-            catch
-            {
-                {
-                    Console.WriteLine("Run (Y for run, N for setup Config)");
-
-                    Console.WriteLine("Y or N: ");
-                    var res = Console.ReadKey();
-                    if (res.KeyChar == 'N' || res.KeyChar == 'n')
-                        File.Delete("setup/config.json");
-
-                    if (!Directory.Exists(Path.Combine(AppContext.BaseDirectory, "setup/")))
-                        Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, "setup/"));
-                }
-
-
-                if (!File.Exists(ConfigPath))
-                {
-                    var cfg = new Config();
-
-                    Console.WriteLine(
-                        "Please go to https://apps.twitter.com/ and create a new app\n" +
-                        "Once Created, go to 'Keys and Access Tokens'\n" +
-                        "Make sure you have generated your access token\n" +
-                        "Press Any Key to Continue");
-                    Console.ReadKey();
-                    Console.Write("Please enter your Consumer Key (API Key): \n");
-                    cfg.ConsumerKey = Console.ReadLine();
-                    Console.Write("Please enter your Consumer Key Secret (API Secret): \n");
-                    cfg.ConsumerKeySecret = Console.ReadLine();
-                    Console.Write("Please enter your Access Token: ");
-                    cfg.AccessToken = Console.ReadLine();
-                    Console.Write("Please enter your Access Token Secret: ");
-                    cfg.AccessTokenSecret = Console.ReadLine();
-
-                    Console.Write("Please enter your Desired RSS Url: ");
-                    cfg.RssUrl = Console.ReadLine();
-
-                    cfg.Save();
                 }
             }
         }
